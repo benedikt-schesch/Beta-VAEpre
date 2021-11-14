7609ee5852d1a3990036b6de557de5d1d422e752
Benedikt <scheschb@student.ethz.ch>
2021-11-14 15:31:34 +0100

    Beta VRE added

---
+++ model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.activation import Softmax
+import utils
+
+
+class BetaVAE(nn.Module):
+
+    def __init__(self,feature_dim,latent_size=8,beta=1):
+        super(BetaVAE, self).__init__()
+
+        self.latent_size = latent_size
+        self.beta = beta
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.LazyLinear(latent_size*4),
+            nn.ReLU()
+        )
+        self.fc_mu = nn.LazyLinear(latent_size)
+        self.fc_var = nn.LazyLinear(latent_size)
+
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.LazyLinear(feature_dim),
+            nn.Softmax()
+        )
+        self.fc_z = nn.Linear(latent_size, latent_size*2)
+
+    def encode(self, x):
+        x = self.encoder(x)
+        return self.fc_mu(x), self.fc_var(x)
+
+    def sample(self, mu, logvar):
+        std = torch.exp(0.5*logvar)  # e^(1/2 * log(std^2))
+        eps = torch.randn_like(std)  # random ~ N(0, 1)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        z = self.fc_z(z)
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.sample(mu, logvar)
+        rx = self.decode(z)
+        return rx, mu, logvar
+
+    def loss(self, recon_x, x, mu, logvar):
+        # reconstruction losses are summed over all elements and batch
+        recon_loss = F.binary_cross_entropy(recon_x, x, reduction='sum')
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        kl_diverge = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return (recon_loss + self.beta * kl_diverge) / x.shape[0]  # divide total loss by batch size
--- train.py
+++ train.py
@@ -1,6 +1,19 @@
+import torch
+import torch.optim
+from  torch.utils.data.dataloader import DataLoader
 import pandas as pd
 import category_encoders as ce
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import numpy as np
+from model import BetaVAE
+from sklearn import preprocessing
 
+
+BATCH_SIZE = 4
+NUM_EPOCHS = 200
+LR = 0.001
+BETA = 1
 
 dataset_path = "xAPI-Edu-Data.csv"
 
@@ -9,6 +22,44 @@ df = pd.read_csv(dataset_path)
 if dataset_path == "xAPI-Edu-Data.csv":
     one_hot_columns = list(df.columns)
     del one_hot_columns[9:13]
+    del one_hot_columns[-1]
     encoder_var = ce.OneHotEncoder(cols=one_hot_columns,return_df=True,use_cat_names=True)
     new_df = encoder_var.fit_transform(df[one_hot_columns])
-    df = new_df.join(df[one_hot_columns[9:13]])
+    feature_df = new_df.join(df[df.columns[9:13]])
+    X = feature_df.to_numpy()
+    Y = (df['Class'] != "L").to_numpy().astype(int)
+
+
+print("Number of datapoint: ",len(X))
+print("Data Imbalance: ",100*(1-sum(Y)/len(Y)),"%")
+
+X = preprocessing.normalize(X)
+X_train, X_test, y_train, y_test = train_test_split(X.astype(np.float32),Y,test_size=0.25, stratify = Y,random_state=0)
+
+at_risk_student_id = np.nonzero(y_train == 0)[0]
+train_loader =  DataLoader([X_train[i] for i in at_risk_student_id], batch_size=BATCH_SIZE)
+
+model = BetaVAE(feature_dim=len(X_train[0]),beta=BETA)
+optimizer = torch.optim.Adam(model.parameters(),lr=LR)
+
+for epoch in range(NUM_EPOCHS):
+    loop = tqdm(train_loader)
+    for idx, x in enumerate(loop):        
+        #reset gradients
+        optimizer.zero_grad()
+        
+        #forward propagation through the network
+        out, mu, logvar = model(x)
+        
+        #calculate the loss
+        loss = model.loss(out, x, mu, logvar)
+        
+        #backpropagation
+        loss.backward()
+        
+        #update the parameters
+        optimizer.step()
+        
+        # add stuff to progress bar in the end
+        loop.set_description(f"Epoch [{epoch}/{NUM_EPOCHS}]")
+        loop.set_postfix(loss=loss.item())
